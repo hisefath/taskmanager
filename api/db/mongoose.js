ae0f19cc6a10cc14bf421c5391caf5003a2d8cdd
shefuchow <mschowdh@buffelo.edu>
2020-07-07 23:25:31 -0400

    dotenv configured

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -1,5 +1,7 @@
+require('../node_modules/dotenv/config');
+
 // This file will handle connection logic to MongoDB Database
 const mongoose = require ('mongoose');
 //override mongoose default bluebird promise and use js global promise
 mongoose.Promise = global.Promise();
-mongoose.connect('didntwork, will udse dotenv');
+mongoose.connect(process.env.SECRET_MONGODB);
