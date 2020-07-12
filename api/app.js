2ff32e6565dd539d6099b9a5c45769f25fff14f5
zaid450 <me@zaid-arshad.tech>
2020-07-12 13:26:45 -0400

    took more of a look and realized it wasn't a good idea to have port rely on env variable

--- api/app.js
+++ api/app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 /* ---Load Mongoose Models--*/
 const {
     List,
